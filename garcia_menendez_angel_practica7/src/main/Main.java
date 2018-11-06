ca763fa8b5063186e77eed951927d0e2b55b90ed
Angel <angelgmsi15@gmail.com>
2018-11-06 23:52:31 +0100

    Practice 7 finished

--- garcia_menendez_angel_practica7/src/main/Main.java
+++ garcia_menendez_angel_practica7/src/main/Main.java
@@ -14,22 +14,27 @@ public class Main {
 		FileSystem system = new FileSystem();
 
 		system.copyFile("privado.txt", new NormalizedTextOutput(new FileOutput("copia.txt")));
-		system.copyFile("privado.txt", new NormalizedTextOutput(new EncriptedOutput(new FileOutput("copiaEncriptada.txt"))));
+		system.copyFile("privado.txt",
+				new NormalizedTextOutput(new EncriptedOutput(new FileOutput("copiaEncriptada.txt"))));
 		system.copyFile("privado.txt", new EncriptedOutput(new Internet("156.35.33.105")));
 		system.copyFile("privado.txt", new NoRepeatedSpacesOutput(new EncriptedOutput(new Internet("156.35.33.106"))));
-		system.copyFile("privado.txt", new NoRepeatedSpacesOutput(new NormalizedTextOutput(new EncriptedOutput(new Internet("156.35.33.107")))));
+		system.copyFile("privado.txt", new NoRepeatedSpacesOutput(
+				new NormalizedTextOutput(new EncriptedOutput(new Internet("156.35.33.107")))));
 		system.copyFile("privado.txt",
 				new EncriptedOutput(new NoRepeatedSpacesOutput(new Bluetooth("iPhone de César"))));
 		CharacterCountOutput counter = new CharacterCountOutput(new Internet("1.1.1.1"));
 		system.copyFile("privado.txt", new EncriptedOutput(counter));
-		System.out.println("The number of characters is: "+counter.getNumberOfCharacters());
-		
-		CharacterCountOutput counter2 = new CharacterCountOutput(new Internet("2.2.2.2") );
-		CharacterCountOutput counter1=new CharacterCountOutput(new NormalizedTextOutput(counter2));
-		
+		System.out.println("The number of characters is: " + counter.getNumberOfCharacters());
+
+		CharacterCountOutput counter2 = new CharacterCountOutput(new Internet("2.2.2.2"));
+		CharacterCountOutput counter1 = new CharacterCountOutput(new NormalizedTextOutput(counter2));
+
 		system.copyFile("privado.txt", counter1);
-		
-		System.out.println("Initially there were "+counter1.getNumberOfCharacters()+" characters");
-		System.out.println("After normalizing there were "+counter2.getNumberOfCharacters()+" characters");
+
+		System.out.println("Initially there were " + counter1.getNumberOfCharacters() + " characters");
+		System.out.println("After normalizing there were " + counter2.getNumberOfCharacters() + " characters");
+
+		system.copyFile("privado.txt",
+				new EncriptedOutput(new DoubleOutput(new Internet("1.1.1.1"), new Bluetooth("iPhone de César"))));
 	}
 }
