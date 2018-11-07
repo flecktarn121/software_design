906bdbd78edfcadcca79bf35da1f0be224d6b7cb
Angel <UO258654@uniovi.es>
2018-11-07 09:17:21 +0100

    Initial materials for practice 8

---
+++ garcia_menendez_angel_practica8/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+import poll.*;
+import ui.Interviewer;
+
+public class Main 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		Poll poll = new Poll("¿Está a favor de la energia nuclear?");
+
+		Interviewer interviewer = new Interviewer();
+		interviewer.fill(poll);
+	}
+}
+
